58d55f168862affb4313bf4ac07efc6f5433aa1a
bjornnewton12 <bjorn.knutsson@edu.newton.se>
2026-05-26 10:23:16 +0200

    Add grid and pagination

--- src/components/CatCard.jsx
+++ src/components/CatCard.jsx
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card';
 
 function CatCard( {cat}) {
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '12rem' }}>
       <Card.Img variant="top" src={`https://cdn2.thecatapi.com/images/${cat.reference_image_id}.jpg`} alt={cat.name} />
       <Card.Body>
         <Card.Title>{cat.name}</Card.Title>
--- src/pages/Cats.jsx
+++ src/pages/Cats.jsx
@@ -1,25 +1,44 @@
 import { useState, useEffect } from "react";
+import Pagination from "react-bootstrap/Pagination";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import CatCard from "../components/CatCard";
 
 function Cats() {
-    const [cats, setCats] = useState([]);
+  const [cats, setCats] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const start = (currentPage - 1) * 10;
+  const end = currentPage * 10;
 
-    useEffect(() => {
+  useEffect(() => {
     fetch(`https://api.thecatapi.com/v1/breeds?limit=30`)
-    .then(res => res.json())
-    .then(data => { 
-        console.log(data[0])
-        setCats(data)})
-    }, []);
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data[0]);
+        setCats(data);
+      });
+  }, []);
 
-    return (
-        <div className='container text-left mt-5'>
-            <h1 className='display-3'>Cats</h1>
-            {cats.map(cat => (
-            <CatCard key={cat.id} cat={cat}/>
+  return (
+    <div className="container text-left mt-5">
+      <h1 className="display-3">Cats</h1>
+      <Row>
+        {cats.slice(start, end).map((cat) => (
+          <Col key={cat.id} xs={3}>
+            <CatCard cat={cat} />
+          </Col>
         ))}
-        </div>
-    )
+      </Row>
+      <Pagination>
+        {Array.from({ length: Math.ceil(cats.length / 10) }, (_, i) => (
+            <Pagination.Item key={i + 1} onClick={() => setCurrentPage(i + 1)}
+            active={currentPage === i + 1}>
+                {i + 1}
+            </Pagination.Item>
+        ))}
+      </Pagination>
+    </div>
+  );
 }
 
-export default Cats
+export default Cats;
