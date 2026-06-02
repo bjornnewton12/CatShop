6438edf23851c459951aa63e86c2cec1dea9aba7
bjornnewton12 <bjorn.knutsson@edu.newton.se>
2026-06-02 09:52:06 +0200

    Add CatCard to shoppingCart

--- src/pages/Cats.jsx
+++ src/pages/Cats.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Pagination from "react-bootstrap/Pagination";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form"
+import Form from "react-bootstrap/Form";
 import CatCard from "../components/CatCard";
 
 function Cats() {
@@ -10,7 +10,7 @@ function Cats() {
   const [currentPage, setCurrentPage] = useState(1);
   const start = (currentPage - 1) * 10;
   const end = currentPage * 10;
-  const [searchQuery, setSearchQuery] = useState("")
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     fetch(`https://api.thecatapi.com/v1/breeds?limit=30`)
@@ -21,28 +21,41 @@ function Cats() {
       });
   }, []);
 
-  const filterCats = cats.filter(cat => cat.reference_image_id)
-  .filter(cat => cat.name.toLowerCase().includes(searchQuery.toLowerCase()))
+  const filterCats = cats
+    .filter((cat) => cat.reference_image_id)
+    .filter((cat) =>
+      cat.name.toLowerCase().includes(searchQuery.toLowerCase()),
+    );
 
   return (
     <div className="container text-left mt-5">
       <h1 className="display-3">Cats</h1>
-      <Form.Control type="text" placeholder="Sök efter katter..." value={searchQuery} 
-        onChange={(e) => {setSearchQuery(e.target.value) 
-        setCurrentPage(1)}} className="mb-4"/>
+      <Form.Control
+        type="text"
+        placeholder="Sök efter katter..."
+        value={searchQuery}
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+          setCurrentPage(1);
+        }}
+        className="mb-4"
+      />
       <Row className="g-4">
         {filterCats.slice(start, end).map((cat) => (
           <Col key={cat.id} xs={12} sm={6} md={4} lg={3}>
-            <CatCard cat={cat}/>
+            <CatCard cat={cat} />
           </Col>
         ))}
       </Row>
       <Pagination className="mt-4">
         {Array.from({ length: Math.ceil(filterCats.length / 10) }, (_, i) => (
-            <Pagination.Item key={i + 1} onClick={() => setCurrentPage(i + 1)}
-            active={currentPage === i + 1}>
-                {i + 1}
-            </Pagination.Item>
+          <Pagination.Item
+            key={i + 1}
+            onClick={() => setCurrentPage(i + 1)}
+            active={currentPage === i + 1}
+          >
+            {i + 1}
+          </Pagination.Item>
         ))}
       </Pagination>
     </div>
--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,34 +1,43 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { useCart } from "../context/CartContext"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import CatCard from "../components/CatCard";
+import { useCart } from "../context/CartContext";
 
 function ShoppingCart() {
-  const { cartItems } = useCart()
+  const { cartItems } = useCart();
   return (
-
     <div className="container text-left mt-5">
-      <h1 className="display-3">Shopping cart</h1>
+      <h1 className="display-3 mb-4">Shopping cart</h1>
       {cartItems.map((cat, index) => (
-      <p key={index}>{cat.name}</p>))}
-    
-    <Form>
-      <Form.Group className="mb-3">
-        <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter name" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Address</Form.Label>
-        <Form.Control type="text" placeholder="Enter address"/>
-      </Form.Group>
+        <div key={index}>
+          <Row className="g-4 mb-4">
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <CatCard cat={cat} />
+            </Col>
+          </Row>
+        </div>
+      ))}
 
-      <Button variant="primary" type="submit">
-        Checkout
-      </Button>
-    </Form>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Address</Form.Label>
+          <Form.Control type="text" placeholder="Enter address" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Checkout
+        </Button>
+      </Form>
     </div>
   );
 }
